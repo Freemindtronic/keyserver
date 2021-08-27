2f1549c67d4eec430059ecb417ac723373ab40d6
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-08-27 15:41:58 +0200

    feat: started lookup user interface

---
+++ src/routes/ui/lookup.svelte
@@ -0,0 +1,29 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit'
+  export const load: Load = async ({ page: { query }, fetch }) => {
+    query.set(
+      'options',
+      query.has('options') ? query.get('options') + ',mr' : 'mr'
+    )
+    const response = await fetch(`/pks/lookup?${query}`)
+
+    return {
+      props: {
+        data: await response.text(),
+      },
+    }
+  }
+</script>
+
+<script lang="ts">
+  export let data: string
+</script>
+
+<p>{data}</p>
+
+<style>
+  p {
+    padding: 0 1em;
+    white-space: pre-line;
+  }
+</style>
